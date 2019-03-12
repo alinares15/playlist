9021db760d45731731bc82d14a1fb86908c32799
Ashley Linares <linaresash@gmail.com>
2019-03-12 22:38:39 +0000

    refractoring

--- js/script.js
+++ js/script.js
@@ -1,40 +1,6 @@
 /* global $ */
 
-// BELOW Update the songs array with four of your favorites songs.
-var songs = ["Poison", "Come on Eileen", "Put your head on my shoulder", "Every little step of the way"];
-// BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
-// Make sure they match the same order as your array above
-var artistnam = ["Bel Biv Devoe", "Dexy's Midnight Runners", "Frank Sinatra", "Bobby Brown"];
 
-var songLength = [261, 274, 2000, 209];
-
-var links = ['https://youtu.be/YejxyaFyUHc', 'https://youtu.be/ASwge9wc-eI', 'https://youtu.be/uvxagNIBVLU', 'https://youtu.be/P0FKzPfsxA4'];
-
-
-var images = ['https://images-na.ssl-images-amazon.com/images/I/61LSLIBWAwL.jpg', 'https://images.genius.com/7c63e499b5783dd3542feedfd20be468.1000x1000x1.jpg', 'https://assets.audiomack.com/mylaaa326/paul-anka-put-your-head-on-my-shoulder-1959-hq-audio-275-275-1516970609.jpg'];
-
-    // BELOW Use forEach Loop to display the data from each of your array's in the correct div
-    songs.forEach(function(title){
-        $("#songs").append("<p>" + title + "</p>");
-    });
-
-    songLength.forEach(function(seconds){
-        $("#lengths").append("<p>" + seconds + "</p>");
-    });
-    
-    artistnam.forEach(function(artists){
-        $("#artists").append("<p>" + artists + "</p>");
-    });
-    
-    links.forEach(function(link){
-        $("#links").append("<a href =" + link + ">" + "Listen" + "</a>");
-    });
-    
-    images.forEach(function(picture){
-        $("#images").append("<img src='" + picture + "'>");    
-    });
-
-    
 function emptySongInfo(){
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs
@@ -45,7 +11,7 @@ function emptySongInfo(){
     var images = $("#images").val();
     
     songs.push(songz);
-    artistnam.push(artistnam);
+    artistname.push(artistname);
     songLength.push(songLength);
     linkz.push(linkz);
     images.push(images);
@@ -59,9 +25,17 @@ function emptySongInfo(){
 }
 
 
-function addSongInfo(){
+function addSongInfo(song){
     // BELOW write the code to add new items to each of the arrays.
-
+   var songName = $('#song').val();
+   var newlistitem = {
+       song: songName,
+       artistName: artistName,
+       songLength: lengths,
+       
+   }
+   playlist.push(newlistitem);
+   playlist.push(newlistitem);
 //.push
 }
 
@@ -74,3 +48,51 @@ $("#add").click(function() {
     
 });
 
+//playlist item then list then present them
+
+var poison = {
+    song: "Poison",
+    artistName: "Bell Biv Devoe",
+    songLength: 261,
+    links: 'https://youtu.be/YejxyaFyUHc',
+    image: "https://images-na.ssl-images-amazon.com/images/I/61LSLIBWAwL.jpg"
+    };
+    
+var comeOnEileen = {
+    song: "Come On Eileen",
+    artistName: "Dexy's Midnight Runners",
+    songLength: 274,
+    links: "https://youtu.be/ASwge9wc-eI",
+    image: "https://images.genius.com/7c63e499b5783dd3542feedfd20be468.1000x1000x1.jpg"
+};
+
+var paulAnka = {
+    song: "Put Your Head on my Shoudler",
+    artistName: "Paul Anka",
+    songLength: 156,
+    links: 'https://youtu.be/uvxagNIBVLU',
+    image: 'https://assets.audiomack.com/mylaaa326/paul-anka-put-your-head-on-my-shoulder-1959-hq-audio-275-275-1516970609.jpg'
+};
+
+var bobbyBrown = {
+    song: "Every Little Step of The Way",
+    artistName: "Bobby Brown",
+    songLength: 209,
+    links: "https://youtu.be/P0FKzPfsxA4",
+    image: "https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2F1c4b7079dcf62b5a837177eb112f6d71.600x600x1.jpg"
+};
+
+var playlist = [
+    poison,
+    comeOnEileen,
+    paulAnka,
+    bobbyBrown
+    ];
+    
+playlist.forEach(function(play){
+    $("#songs").append("<p>" + play.song + "</p>");
+    $("#artists").append("<p>" + play.artistName + "</p>");
+    $("#lengths").append("<p> " + play.songLength + "</p>");
+    $("#links").append("<a href='" + play.links + "'> click </a>");
+    $("#images").append("<img src=' " + play.image + "'>");
+});
